4af962170ef8b783caef4b977df9832bf066ae99
Larissa  Moreno Silva <56546860+LarissaMoreno@users.noreply.github.com>
2022-04-27 12:20:40 -0300

    Update evaluate-sistem.R

--- evaluate-sistem.R
+++ evaluate-sistem.R
@@ -18,20 +18,27 @@ rating$user_id <- as.factor(rating$user_id)#transformando os user_id em factor
 ratingMatrix <- as(rating, "realRatingMatrix")
 
 
-#filtrando apenas os animes que foram visto pelo menos 150 vezes 
-#e usuarios que avaliaram pelo menos 200 animes
-ratingMatrix = ratingMatrix[rowCounts(ratingMatrix)>=200 , colCounts(ratingMatrix)>=150]
+#filtrando apenas os animes que foram visto pelo menos 100 vezes 
+#e usuarios que avaliaram pelo menos 50 animes
+ratingMatrix = ratingMatrix[rowCounts(ratingMatrix)>=50 , colCounts(ratingMatrix)>=100]
+
+#numero de animes 4143 numero de usuários 32967
+dim(ratingMatrix)
+4143/length(unique(rating$anime_id))
+32967/length(unique(rating$user_id))
+
+length(ratingMatrix@data@x)
+length(ratingMatrix@data@x)/(4143*32967)
+
 
 percentage_training <- 0.8#80% para base de treinammento
 min(rowCounts(ratingMatrix))#número mínimo de itens adquiridos por qualquer usuário 
 
-#Para cada usuário no conjunto de teste, precisamos definir quantos itens usar para gerar recomendações
-items_to_keep <- 20#normalmente deve ser menor que o numero minimo de itens por usuario
-
 
 #cross-validation
+set.seed(5)
 eval_sets <- evaluationScheme(data = ratingMatrix, method = "split",
-                              train = percentage_training, given = items_to_keep, goodRating =5
+                              train = 0.8, given = 2, goodRating =5
                                 ) 
 
 
@@ -44,7 +51,7 @@ IBCF_prediction <- predict(object = IBCF_model, newdata =
                              getData(eval_sets, "known"), n = 10, type = "ratings")
 
 
-#top 10 recomendações para usuario 129
+#top 10 recomendações para usuario 17
 x=data.frame(as(IBCF_prediction, 'list')[2])
 x$anime_id=as.integer(rownames(x))
 x=arrange(x,desc(x$X129))%>%head(10)
@@ -61,15 +68,15 @@ head(IBCF_accuracy)
 
 
 #modelo baseado na filtragem colaborativa por usuario
-UBCF_model <- Recommender(data = getData(eval_sets, "train"), method = "UBCF")
+UBCF_model <- Recommender(data = getData(eval_sets, "train"), method = "UBCF",param=list(method="pearson",nn=25))
 
 UBCF_prediction <- predict(object = UBCF_model, newdata =
                              getData(eval_sets, "known"), n = 10, type = "ratings")
 
-#top 10 recomendações para usuario 129
-x1=data.frame(as(UBCF_prediction, 'list')[2])
+#top 10 recomendações para usuario 17
+x1=data.frame(as(UBCF_prediction, 'list')[1])
 x1$anime_id=as.integer(rownames(x1))
-x1=arrange(x1,desc(x1$X129))%>%head(10)
+x1=arrange(x1,desc(x1$X17))%>%head(10)
 x1=inner_join(x1,anime,by="anime_id")%>%select(c(1,3))
 colnames(x1)=c(paste0("rating de ",colnames(x1)[1]),"name")
 x1
@@ -89,14 +96,13 @@ POP_model <- Recommender(data = getData(eval_sets, "train"), method = "POPULAR")
 POP_prediction <- predict(object = POP_model, newdata =
                              getData(eval_sets, "known"), n = 10, type = "ratings")
 
-#top 10 recomendações para usuario 129
-x2=data.frame(as(POP_prediction, 'list')[2])
+#top 10 recomendações para usuario 17
+x2=data.frame(as(POP_prediction, 'list')[1])
 x2$anime_id=as.integer(rownames(x2))
-x2=arrange(x2,desc(x2$X129))%>%head(10)
+x2=arrange(x2,desc(x2$X17))%>%head(10)
 x2=inner_join(x2,anime,by="anime_id")%>%select(c(1,3))
 colnames(x2)=c(paste0("rating de ",colnames(x2)[1]),"name")
 x2
-
 
 
 #RMSE MSE MAE
@@ -117,7 +123,7 @@ x3
 #Matriz de confução 
 models_evaluate <- list(
   IBCF = list(name = "IBCF"),
-  UBCF=list(name="UBCF"),
+  UBCF=list(name="UBCF",param=list(method="pearson",nn=25)),
   POPULAR = list(name = "POPULAR"))
 results <- evaluate(x = eval_sets, method =  models_evaluate, n =seq(10, 100, 10))
 #n: numero de recomendação para cada usuaário
@@ -131,8 +137,12 @@ results[["POPULAR"]]@results[[1]]@cm
 
 #Não sei pra que que serve ainda
 plot(results, annotate = F, legend = "topleft",xlab="Especificidade", ylab="Sensibilidade") 
+abline(coef = c(0,1),lty=2,col="grey")
 title("ROC Curve")
 
 
 plot(results, "prec/rec", annotate = T, legend = "bottomright",xlab="Sensibilidade",ylab="Precisão")
 title("Pecisão-Sensibilidade")
+
+
+
